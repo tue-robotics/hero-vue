4b7e1a8f53d67199b95094c345ccf09dd577d3d1
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-12-03 22:18:11 +0100

    convert into VUE sfc lib

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { HeroVueSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    HeroVueSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <hero-vue-sample />
+  </div>
+</template>
