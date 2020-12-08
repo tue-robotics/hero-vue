4445181881c2aa7481a394932cae48178ac30453
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-12-08 22:53:08 +0100

    WIP: add tests

--- tests/unit/battery.spec.js
+++ tests/unit/battery.spec.js
@@ -3,14 +3,59 @@ import { mount } from '@vue/test-utils'
 import AutoRos from 'auto-ros'
 import Battery from '@/components/battery.vue'
 
+import Vue from 'vue'
+// import { random } from 'Math'
+
 describe('battery.vue', () => {
-  it('renders props.msg when passed', () => {
-    const wrapper = mount(Battery, {
-      propsData: {
-        ros: AutoRos.ros
-      }
+  const wrapper = mount(Battery, {
+    propsData: {
+      ros: AutoRos.ros
+    }
+  })
+  beforeEach(() => {
+    for (const prop of Object.getOwnPropertyNames(wrapper.vm.$data.batteries)) {
+      delete wrapper.vm.$data.batteries[prop]
+    }
+  })
+
+  it('Batteries should be empty on startup', () => {
+    expect(Object.keys(wrapper.vm.$data.batteries)).to.have.lengthOf(0)
+    expect(wrapper.findAll('battery_col')).to.have.lengthOf(0)
+  })
+
+  it('Batteries should be alphabetical', () => {
+    wrapper.vm.handleBatteryMsg({ percentage: 1.0, location: 'hero2', power_supply_status: 1 })
+    wrapper.vm.handleBatteryMsg({ percentage: 1.0, location: 'hero1', power_supply_status: 1 })
+    expect(Object.keys(wrapper.vm.$data.batteries)).to.deep.equal(['hero1', 'hero2'])
+  })
+
+  it('Two battery_col should be rendered alphabetical', () => {
+    wrapper.vm.handleBatteryMsg({ percentage: 1.0, location: 'hero2', power_supply_status: 1 })
+    wrapper.vm.handleBatteryMsg({ percentage: 1.0, location: 'hero1', power_supply_status: 1 })
+    return Vue.nextTick().then(() => {
+      const batteryCols = wrapper.findAll('#battery_col')
+      expect(batteryCols).to.have.lengthOf(2)
+      expect(batteryCols.at(0).get('h5').text()).to.equal('hero1')
+      expect(batteryCols.at(1).get('h5').text()).to.equal('hero2')
     })
-    console.log(wrapper)
-    expect(true)
+  })
+
+  it('Percentage should be in [0, 100]', () => {
+    for (var i = 0; i < 10; i++) {
+      wrapper.vm.handleBatteryMsg({ percentage: Math.random(), location: 'hero1', power_supply_status: 1 })
+      expect(wrapper.vm.$data.batteries.hero1.percentage).to.be.at.least(0).and.at.most(100)
+    }
+  })
+
+  it('Progress color should match battery percentage', () => {
+    var promises = []
+    for (var i = 0; i < 10; i++) {
+      wrapper.vm.handleBatteryMsg({ percentage: Math.random(), location: 'hero1', power_supply_status: 1 })
+      promises.push(Vue.nextTick().then(() => {
+        // console.log(wrapper.b-progress-bar
+        expect(false)
+      }))
+    }
+    return promises
   })
 })
--- tests/unit/indicator.spec.js
+++ tests/unit/indicator.spec.js
@@ -4,20 +4,47 @@ import AutoRos from 'auto-ros'
 import { BButton } from 'bootstrap-vue'
 import Indicator from '@/components/indicator.vue'
 
+import Vue from 'vue'
+
 describe('indicator.vue', () => {
   const wrapper = mount(Indicator, {
     propsData: {
       ros: AutoRos.ros
     }
   })
+  const button = wrapper.getComponent(BButton)
 
-  it('type should be "dark" on startup.', () => {
+  it('Type should be "dark" on startup.', () => {
     expect(wrapper.vm.$data.type).to.be.a('string')
     expect(wrapper.vm.$data.type).to.equal('dark')
+    expect(button.classes()).to.include('btn-dark')
   })
 
   it('Button should be disabled', () => {
-    const button = wrapper.getComponent(BButton)
     expect(button.classes()).to.include('disabled')
+  })
+
+  it('Receive false, button should be green', () => {
+    wrapper.vm.handleMsg({ data: false })
+    expect(wrapper.vm.$data.type).to.equal('success')
+    return Vue.nextTick().then(() => {
+      expect(button.classes()).to.include('btn-success')
+    })
+  })
+
+  it('Receive true, button should be red', () => {
+    wrapper.vm.handleMsg({ data: true })
+    expect(wrapper.vm.$data.type).to.equal('danger')
+    return Vue.nextTick().then(() => {
+      expect(button.classes()).to.include('btn-danger')
+    })
+  })
+
+  it('ROS closes, button should be "dark"', () => {
+    AutoRos.ros.emit('close')
+    expect(wrapper.vm.$data.type).to.equal('dark')
+    return Vue.nextTick().then(() => {
+      expect(button.classes()).to.include('btn-dark')
+    })
   })
 })
