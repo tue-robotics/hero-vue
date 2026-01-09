aa6fe1b689ee9985d5870518bad7345d0bb2791d
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2026-01-09 11:46:07 +0100

    Add polyfills to externalize os, dep of auto-ros

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // Vite config for DEVELOPMENT MODE ONLY
 // Production builds use pure Rollup (see build/rollup.config.js)
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    nodePolyfills({
+      include: ['os']
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
